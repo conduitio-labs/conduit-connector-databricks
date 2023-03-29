6e9ef4ca3b17032a9625993875ab2c4ba982a118
AdamHaffar <haffaradam@yahoo.com>
2023-03-29 16:07:02 +0100

    implement getColumnInfo() readPayload() PrepareSQl and BuildSQL.
    this commit takes a record and sets up a base SQL statement using queried columns with the help of squirrel. The sql INSERT statement is then built and executed through databricks db sql go driver.

--- client.go
+++ client.go
@@ -19,11 +19,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/Masterminds/squirrel"
 	"strings"
 	"time"
 
 	sdk "github.com/conduitio/conduit-connector-sdk"
-	dbsql "github.com/databricks/databricks-sql-go"
+	mysql "github.com/databricks/databricks-sql-go"
 	"github.com/rs/zerolog"
 )
 
@@ -36,8 +37,14 @@ func init() {
 const ansiMode = "ansi_mode"
 
 type sqlClient struct {
-	db        *sql.DB
-	TableName string
+	db             *sql.DB
+	TableName      string
+	AllColumnInfos []map[string]ColumnInfo
+}
+
+type ColumnInfo struct {
+	dataType string
+	value    interface{}
 }
 
 func newClient() *sqlClient {
@@ -47,12 +54,12 @@ func newClient() *sqlClient {
 func (c *sqlClient) Open(ctx context.Context, config Config) error {
 	sdk.Logger(ctx).Debug().Msg("opening sql client")
 
-	connector, err := dbsql.NewConnector(
-		dbsql.WithAccessToken(config.Token),
-		dbsql.WithServerHostname(config.Host),
-		dbsql.WithPort(config.Port),
-		dbsql.WithHTTPPath(config.HTTPath),
-		dbsql.WithSessionParams(map[string]string{
+	connector, err := mysql.NewConnector(
+		mysql.WithAccessToken(config.Token),
+		mysql.WithServerHostname(config.Host),
+		mysql.WithPort(config.Port),
+		mysql.WithHTTPPath(config.HTTPath),
+		mysql.WithSessionParams(map[string]string{
 			ansiMode: "true",
 		}),
 	)
@@ -70,6 +77,11 @@ func (c *sqlClient) Open(ctx context.Context, config Config) error {
 	c.TableName = config.TableName
 	if err != nil {
 		return fmt.Errorf("failed to get field count: %w", err)
+	}
+
+	err = c.getColumnInfo()
+	if err != nil {
+		return fmt.Errorf("unable to get column information: %w", err)
 	}
 
 	sdk.Logger(ctx).Debug().Msg("sql client opened")
@@ -105,29 +117,56 @@ func (c *sqlClient) HandleRecord(ctx context.Context, record sdk.Record) error {
 func (c *sqlClient) Insert(ctx context.Context, record sdk.Record) error {
 	sdk.Logger(ctx).Info().Msg("inserting record")
 
-	//prepare SQL statement
-	sql, vals, err := c.PrepareSQL(ctx, record)
+	var keys []string
+	var values []interface{}
+	var types []string
+
+	var err error
+	c.AllColumnInfos, err = c.readPayload(c.AllColumnInfos, record)
 	if err != nil {
 		return err
 	}
-	sdk.Logger(ctx).Info().Msgf("\n::::::::::%v\n", sql)
 
-	stmt, err := c.db.Prepare(sql)
-	if err != nil {
-		return fmt.Errorf("failed to prepare db sql: %w", err)
+	for _, col := range c.AllColumnInfos {
+		for key, info := range col {
+			keys = append(keys, key)
+			values = append(values, info.value)
+			types = append(types, info.dataType)
+		}
 	}
-	defer stmt.Close()
 
-	//_, err = stmt.Exec(vals...)
-	_, err = stmt.Exec("DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT", "DEFAULT")
+	//prepare SQL statement
+	statement, err := c.PrepareSQL(ctx, keys, values)
 	if err != nil {
-		return fmt.Errorf("failed to execute db sql: %w %v", err, vals)
+		return err
+	}
+
+	//build SQL statement
+	statement, err = c.BuildSQL(statement, values, types)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := c.db.Prepare(statement)
+	if err != nil {
+		return fmt.Errorf("failed to prepare db statement: %w", err)
+	}
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+
+		}
+	}(stmt)
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return fmt.Errorf("failed to execute db statement: %w ", err)
 	}
 
 	return nil
 }
 
-func (c *sqlClient) Update(ctx context.Context, record sdk.Record) error {
+func (c *sqlClient) Update(context.Context, sdk.Record) error {
 
 	return nil
 }
@@ -142,48 +181,102 @@ func (c *sqlClient) Snapshot(ctx context.Context, record sdk.Record) error {
 	return nil
 }
 
-func (c *sqlClient) PrepareSQL(ctx context.Context, record sdk.Record) (string, []interface{}, error) {
+// getColumnInfo gets information on all the column names and types and stores them
+func (c *sqlClient) getColumnInfo() error {
 
-	payload := make(sdk.StructuredData)
-	if err := json.Unmarshal(record.Payload.After.Bytes(), &payload); err != nil {
-		return "", nil, fmt.Errorf("error unmarshalling: %w", err)
+	var ignoredValue sql.NullString
+
+	describeQuery := "DESCRIBE " + c.TableName
+
+	rows, err := c.db.Query(describeQuery)
+	if err != nil {
+		return fmt.Errorf("failed to execute describe query: %v", err)
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	for rows.Next() {
+		newCol := map[string]ColumnInfo{}
+		var colName string
+		var colType string
+		err := rows.Scan(&colName, &colType, &ignoredValue)
+		if err != nil {
+			return fmt.Errorf("failed to next(): %v", err)
+		}
+		newCol[colName] = ColumnInfo{
+			dataType: colType,
+			value:    nil,
+		}
+
+		c.AllColumnInfos = append(c.AllColumnInfos, newCol)
 	}
 
-	sdk.Logger(ctx).Info().Msgf("\n||||||||||||%v\n", payload)
-
-	var columns []string
-	var placeholders []string
-	var vals []interface{}
-
-	//iterate through payload and append values to new slice
-	for col, val := range payload {
-		columns = append(columns, col)
-		placeholders = append(placeholders, "?")
-		vals = append(vals, val)
-	}
-
-	//construction of SQL statement
-	return fmt.Sprintf(
-			"INSERT INTO %s (%s) VALUES (%s)",
-			c.TableName,
-			strings.Join(columns, ", "),
-			strings.Join(placeholders, ", ")),
-		vals, nil
-
+	return nil
 }
 
-func (c *sqlClient) GetFieldCount(config Config) (int, error) {
+func (c *sqlClient) readPayload(AllColumns []map[string]ColumnInfo, record sdk.Record) ([]map[string]ColumnInfo, error) {
+	payload := make(sdk.StructuredData)
+	if err := json.Unmarshal(record.Payload.After.Bytes(), &payload); err != nil {
+		return nil, fmt.Errorf("error unmarshalling: %w", err)
+	}
 
-	columns, err := c.db.Query("SHOW COLUMNS FROM " + config.TableName)
+	for _, columnInfoMap := range AllColumns {
+		for key, columnInfo := range columnInfoMap {
+			if value, ok := payload[key]; ok {
+				columnInfo.value = value
+				columnInfoMap[key] = columnInfo
+			}
+		}
+	}
+
+	return AllColumns, nil
+}
+
+func (c *sqlClient) PrepareSQL(ctx context.Context, keys []string, values []interface{}) (string, error) {
+
+	sqlString, _, err := squirrel.
+		Insert(c.TableName).
+		Columns(keys...).
+		Values(values...).
+		ToSql()
 	if err != nil {
-		return 0, fmt.Errorf("failed to execute query: %w", err)
+		return "", fmt.Errorf("error creating sqlString: %w", err)
 	}
 
-	//Iterate through row object to count number of columns
-	count := 0
-	for columns.Next() {
-		count++
+	sdk.Logger(ctx).Info().Msgf(" \n 0000000%v\n", sqlString)
+
+	return sqlString, nil
+}
+
+func (c *sqlClient) BuildSQL(sql string, values []interface{}, types []string) (string, error) {
+
+	if len(values) != len(types) {
+		return "", fmt.Errorf("values and types slices should have the same length")
 	}
 
-	return count, nil
+	formattedValues := make([]string, len(values))
+	for i, value := range values {
+		formattedValues[i] = fmt.Sprintf("try_cast(\"%v\" as %s)", value, types[i])
+	}
+
+	placeholders := strings.Count(sql, "?")
+	if placeholders != len(values) {
+		return "", fmt.Errorf("number of placeholders in sql string should match the number of values")
+	}
+
+	sqlParts := strings.SplitN(sql, "?", placeholders+1)
+	rewrittenSQL := strings.Builder{}
+
+	for i, sqlPart := range sqlParts[:placeholders] {
+		rewrittenSQL.WriteString(sqlPart)
+		rewrittenSQL.WriteString(formattedValues[i])
+	}
+
+	rewrittenSQL.WriteString(sqlParts[placeholders])
+
+	return rewrittenSQL.String(), nil
 }
